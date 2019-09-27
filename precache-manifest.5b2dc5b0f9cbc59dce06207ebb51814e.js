c56eff57f8c4489f925c0647795423cb3e11f6a5
unknown <brilliancegold1616@gmail.com>
2019-09-28 00:30:37 +0300

    Updates

---
+++ precache-manifest.5b2dc5b0f9cbc59dce06207ebb51814e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502d8e685cac4b3890143fe9487aa388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb325ef3195b1b1af829",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "c0fe259279a1bf88536b",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "bb325ef3195b1b1af829",
+    "url": "/static/js/2.9e7e82e0.chunk.js"
+  },
+  {
+    "revision": "c0fe259279a1bf88536b",
+    "url": "/static/js/main.3e4353e7.chunk.js"
+  },
+  {
+    "revision": "bd544d3d6614ceb3a576",
+    "url": "/static/js/runtime-main.ec02272e.js"
+  },
+  {
+    "revision": "bbebee0321995c5a8f789241f93be98b",
+    "url": "/static/media/LD wo bg.bbebee03.png"
+  }
+]);
